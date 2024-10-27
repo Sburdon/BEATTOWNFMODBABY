3b3f65ff3dafb002d7990c322c488f24e2b1b0d0
Brady09 <bradysutton24@gmail.com>
2024-10-27 15:14:04 -0400

    Setting Up Animation Correctly can build Exe

--- BeatTown Milestone 2/Assets/CurrentScripts/StateMachine.cs
+++ BeatTown Milestone 2/Assets/CurrentScripts/StateMachine.cs
@@ -2,19 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "WrestlerAnimationSet", menuName = "Animation/WrestlerAnimationSet", order = 1)]
-public class WrestlerAnimationSet : ScriptableObject
-{
-    public Texture2D idleTexture;
-    public Texture2D moveTexture;
-    public Texture2D punchTexture;
-    public Texture2D swingTexture;
-    public Texture2D pushTexture;
-    public Texture2D reactTexture;
-
-    public Texture2D sourceTexture;
-    public Texture2D baseMask;  // common base mask if applicable for all animations
-}
 
 public class StateMachine : MonoBehaviour
 {
