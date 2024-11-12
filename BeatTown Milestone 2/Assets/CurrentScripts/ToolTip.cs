713e1e57b8c8b744f8f02f7f13aa7d265118e3a7
rubroshe <rubroshe@iu.edu>
2024-11-12 12:45:48 -0500

    ToolTip scripts archived

--- BeatTown Milestone 2/Assets/CurrentScripts/ToolTip.cs
+++ BeatTown Milestone 2/Assets/CurrentScripts/ToolTip.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class ToolTip : MonoBehaviour
+public class ToolTip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    public string message;
+
+    public string actionName;
+    public string actionCost;
+    public string actionDescription;
+    public string keywords;
+
+
+    //=======================================For GameObjects==================
+    /*private void OnMouseEnter()
     {
-        
+        ToolTipManager._instance.SetAndShowToolTip(message);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseExit()
     {
-        
+        ToolTipManager._instance.HideToolTip();
+    }*/
+    //=======================================For Buttons=========================
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    { // When the mouse enters the object, show the tooltip
+        ToolTipManager._instance.SetAndShowToolTip(message);
+    }
+
+    public void OnPointerExit(PointerEventData pointerEventData)
+    { // When the mouse exits the object, hide the tooltip
+        ToolTipManager._instance.HideToolTip();
     }
 }
--- BeatTown Milestone 2/Assets/CurrentScripts/ToolTipManager.cs
+++ BeatTown Milestone 2/Assets/CurrentScripts/ToolTipManager.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ToolTipManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public static ToolTipManager _instance; // Singleton instance of the tooltip manager
+
+    public TextMeshProUGUI textComponent; // The text component of the tooltip (drag in text box for relevant tooltip)
+
+    private void Awake()
     {
-        
+        if (_instance != null && _instance != this)
+        { // If there is already an instance of the tooltip manager, destroy this one
+            Destroy(this.gameObject);
+        }
+        else
+        { // Otherwise, set this as the instance
+            _instance = this;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        Cursor.visible = true; // Make the cursor visible
+        gameObject.SetActive(false); // Hide the tooltip
+    }
+
+    private void Update()
+    {
+        transform.position = Input.mousePosition; // Update the position of the tooltip to the mouse position
+    }
+
+    public void SetAndShowToolTip(string messsage) // Set the tooltip message and show the tooltip
+    {
+        gameObject.SetActive(true);
+        textComponent.text = messsage;
+    }
+
+    public void HideToolTip() // Hide the tooltip
+    {
+        gameObject.SetActive(false);
+        textComponent.text = string.Empty;
     }
 }
