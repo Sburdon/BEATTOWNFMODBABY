af2b10a95fa7de9f6e1d101cc2a038d0b87bfa43
rubroshe <rubroshe@iu.edu>
2024-12-07 00:24:14 -0500

    FULL TUTORIAL

--- BeatTown Milestone 2/Assets/CurrentScripts/Tutorial.cs
+++ BeatTown Milestone 2/Assets/CurrentScripts/Tutorial.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Tutorial : MonoBehaviour
 {
      Animator animtut;
+    public Button[] barray;
 
     private void Start()
     {
         animtut = GetComponent<Animator>();
 
-
+        foreach(Button b in barray)
+        {
+            b.interactable = false;
+        }
+        Time.timeScale = 0f;
     }
 
     void ChangeAnimation()
@@ -18,6 +24,14 @@ public class Tutorial : MonoBehaviour
         animtut.SetInteger("Change", animtut.GetInteger("Change") + 1);
     }
 
+    public void Etut()
+    {
+        foreach (Button b in barray)
+        {
+            b.interactable = true;
+        }
+        Time.timeScale =1f ;
+    }
 
     private void Update()
     {
@@ -26,4 +40,6 @@ public class Tutorial : MonoBehaviour
             ChangeAnimation();
         }
     }
+
+
 }
