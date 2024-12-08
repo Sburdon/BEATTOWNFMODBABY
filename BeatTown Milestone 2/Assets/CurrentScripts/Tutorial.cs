a4717c6a69ab304e54fd40e6e81d5bf043cf08d7
rubroshe <rubroshe@iu.edu>
2024-12-08 18:14:10 -0500

    Tooltips and Tutorial Finish

--- BeatTown Milestone 2/Assets/CurrentScripts/Tutorial.cs
+++ BeatTown Milestone 2/Assets/CurrentScripts/Tutorial.cs
@@ -12,19 +12,31 @@ public class Tutorial : MonoBehaviour
     {
         animtut = GetComponent<Animator>();
 
-        foreach(Button b in barray)
+        // Disable button interactions
+        foreach (Button b in barray)
         {
             b.interactable = false;
         }
+
+        // start coroutiune to delay setting Time.timescale to 0
+        StartCoroutine(DelayPause());
+    }
+
+    private IEnumerator DelayPause()
+    {
+        // Wait for 2 seconds (or any desired duration)
+        yield return new WaitForSecondsRealtime(2f); // Use WaitForSecondsRealtime to account for timeScale being 1
+
+        // Pause the game
         Time.timeScale = 0f;
     }
 
-    void ChangeAnimation()
+    void ChangeAnimation() // to cycle through tutorial animations
     {
         animtut.SetInteger("Change", animtut.GetInteger("Change") + 1);
     }
 
-    public void Etut()
+    public void Etut() // end tutorial (called in Animator)
     {
         foreach (Button b in barray)
         {
@@ -33,7 +45,7 @@ public class Tutorial : MonoBehaviour
         Time.timeScale =1f ;
     }
 
-    private void Update()
+    private void Update() // any key advances tutorial
     {
         if (Input.anyKeyDown)
         {
