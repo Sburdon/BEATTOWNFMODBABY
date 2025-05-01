6e51eada0642e537ccdcf91702be0220d45f62cb
Brady09 <bradysutton24@gmail.com>
2025-05-01 01:03:34 -0400

    Blood Water

--- BeatTown Milestone 2/Assets/CurrentScripts/Obsticals/Hook.cs
+++ BeatTown Milestone 2/Assets/CurrentScripts/Obsticals/Hook.cs
@@ -14,6 +14,7 @@ public class Hook : MonoBehaviour
     public delegate void HookKillCountChangedEvent(int killCount);
     public static event HookKillCountChangedEvent OnHookKillCountChanged;
 
+
     [Header("References")]
     public Tilemap tilemap;
     public PlayerMove player;
@@ -39,6 +40,7 @@ public class Hook : MonoBehaviour
             return;
         }
     }
+   
 
     void Start()
     {
